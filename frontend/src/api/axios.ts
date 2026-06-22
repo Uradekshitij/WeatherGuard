bbc1a679478149077b23f49f1460e8f708c2f611
Kshitij Urade <kshitijurade33@gmail.com>
2026-06-22 15:06:11 +0530

    Update production API URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL: 'http://localhost:3000',
+baseURL: 'https://weatherguard-backend-9wmx.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
